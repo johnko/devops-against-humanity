f6e8d834aa66c15153745af0b1b273c128937377
johnko <git@johnko.ca>
2018-05-30 00:11:36 -0400

    remove csv, add separate sorted white and black cards with more new cards, add local sample script

---
+++ sample-local.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import random,re
+
+white=[]
+for q in open('./cards-white.txt', 'rb'):
+    white.append(q)
+black=[]
+for r in open('./cards-black.txt', 'rb'):
+    black.append(r)
+
+blank=re.compile(r"( +)?(\b|[^_])_+(\b|[^_])( +)?")
+rpl=lambda _:" %s " % random.choice(white).strip()
+for _ in range(10):
+    print(re.sub(blank,rpl,random.choice(black)).strip().replace(' .','.'))
